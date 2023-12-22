dcf769581c8899b3631d97ef1fc0c177b9dacb97
Ivan A <bladee.tr@yandex.ru>
2023-12-22 05:29:31 -0800

    loss function added.

---
+++ preprocessing/extract_data.py
@@ -0,0 +1,18 @@
+
+import torch
+import torchvision
+import os
+import json
+
+class LoadData:
+    def __init__(self):
+        pass
+
+
+
+    @staticmethod
+    def extract_labels(self, filename):
+
+
+
+
