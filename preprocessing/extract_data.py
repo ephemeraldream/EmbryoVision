5419c3d261de0827985b8b0e86b8cdb52f36f033
Ivan A <bladee.tr@yandex.ru>
2023-12-24 02:25:01 -0800

    1

--- preprocessing/extract_data.py
+++ preprocessing/extract_data.py
@@ -11,8 +11,28 @@ class LoadData:
 
 
     @staticmethod
-    def extract_labels(self, filename):
+    def extract_labels(filename):
+        pass
 
 
 
 
+    @staticmethod
+    def extract_images(directory_in_str):
+        directory = os.fsencode(directory_in_str)
+        dir_of_images = dict()
+        for file in os.listdir(directory):
+            raw_filename = str(os.fsdecode(file))[:-11]
+            filename_torch = directory_in_str + str(os.fsencode(file))[2:-1]
+            img = torchvision.io.read_image(filename_torch)
+            dir_of_images[raw_filename] = img
+        torch.save(directory_in_str, "C:\\Work\\EmbryoVision\\data\\torch_type\\first_pack")
+
+
+def main():
+    load = LoadData
+    #load.extract_images('C:\\Work\\EmbryoVision\\data\\images\\')
+
+
+if __name__ == "__main__":
+    main()
