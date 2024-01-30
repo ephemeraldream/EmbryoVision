8f27e55f8810c91a2413af5e710225f89614ae6a
Ivan A <bladee.tr@yandex.ru>
2024-01-30 04:40:36 -0700

    another

--- depdl/deepapp/models.py
+++ depdl/deepapp/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=2000)
-    pub_date = models.DateTimeField("date published")
-
-    def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=2000)
-    votes = models.IntegerField(default=0)
-
 
 class Image(models.Model):
     image = models.ImageField(upload_to="images/")
--- depdl/depdl/settings.py
+++ depdl/depdl/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'depdl.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "embryovision",
+        "USER": "cyberbiba2000",
+        "PASSWORD": "12345",
+        "HOST": "localhost",
+        "PORT": 5432,
+    },
 }
 
 
